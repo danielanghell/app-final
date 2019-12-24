96f1085d5ced98bac2e19ab63ec0bc520c1a618e
daniellanghel <dubskate_danii@yahoo.com>
2019-12-24 13:55:47 +0200

    v1.4-faravalid

--- tvseries/src/app/shared-components/register/register.component.ts
+++ tvseries/src/app/shared-components/register/register.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'register',
@@ -7,8 +9,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  get userName() {
+    return this.registrationForm.get('userName');
+  }
 
-  ngOnInit() {}
+  get password() {
+    return this.registrationForm.get('password');
+  }
+
+
+  constructor(private fb: FormBuilder, private router: Router) { }
+
+  registrationForm = this.fb.group({
+    userName: ['', [Validators.required, Validators.minLength(3)]],
+    password: ['', [Validators.required, Validators.minLength(8)]],
+    address: [''],
+    phone: ['']
+  });
+
+
+  //loginForm = new FormGroup({
+  //userName: new FormControl(''),
+  //password: new FormControl('')
+  //});
+
+  ngOnInit() { }
 
 }
